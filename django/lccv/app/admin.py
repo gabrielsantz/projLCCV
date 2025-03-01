59aca9e3f7bffeb1c4cd2988a043937993874ee5
gabriel <vcgabriel@outlook.com>
2025-03-01 17:17:17 -0300

    add admin

--- django/lccv/app/admin.py
+++ django/lccv/app/admin.py
@@ -1,3 +1,25 @@
 from django.contrib import admin
+from .models import Colaboradores, Financiador, AreasTecnologicas, Projetos
 
-# Register your models here.
+@admin.register(Colaboradores)
+class ColaboradoresAdmin(admin.ModelAdmin):
+    list_display = ('id_colaborador', 'nome', 'cpf', 'dt_nascimento')
+    search_fields = ('nome', 'cpf')
+    list_filter = ('dt_nascimento',)
+
+@admin.register(Financiador)
+class FinanciadorAdmin(admin.ModelAdmin):
+    list_display = ('id_financiador', 'financiador')
+    search_fields = ('financiador',)
+
+@admin.register(AreasTecnologicas)
+class AreasTecnologicasAdmin(admin.ModelAdmin):
+    list_display = ('id_area_tecnologica', 'area_tecnologica')
+    search_fields = ('area_tecnologica',)
+
+@admin.register(Projetos)
+class ProjetosAdmin(admin.ModelAdmin):
+    list_display = ('id_projeto', 'projeto', 'id_financiador', 'id_area_tecnologica', 'coordenador', 'ativo', 'inicio_vigencia', 'fim_vigencia', 'valor')
+    search_fields = ('projeto', 'coordenador')
+    list_filter = ('id_financiador', 'id_area_tecnologica', 'inicio_vigencia')
+    filter_horizontal = ('equipe',)
--- django/lccv/app/models.py
+++ django/lccv/app/models.py
@@ -2,43 +2,59 @@ from django.db import models
 
 # Create your models here.
 class Colaboradores(models.Model):
-    id_colaborador = models.IntegerField(primary_key=True)
-    cpf = models.CharField(max_length=11, unique=True)
-    nome = models.CharField(max_length=100)
-    dt_nascimento = models.DateField()
+    id_colaborador = models.IntegerField(primary_key=True, verbose_name='ID do Colaborador')
+    cpf = models.CharField(max_length=11, unique=True, verbose_name='CPF')
+    nome = models.CharField(max_length=100,verbose_name='Nome')
+    dt_nascimento = models.DateField(verbose_name='Data de Nascimento')
     
     def __str__(self):
         return self.nome
+    
+    class Meta:
+        verbose_name = 'Colaborador'
+        verbose_name_plural = 'Colaboradores'
 
 class Financiador(models.Model):
-    id_financiador = models.IntegerField(primary_key=True)
-    financiador = models.CharField(max_length=100)
+    id_financiador = models.IntegerField(primary_key=True, verbose_name='ID do Financiador')
+    financiador = models.CharField(max_length=100, verbose_name='Financiador')
 
     def __str__(self):
         return self.financiador
+    
+    class Meta:
+        verbose_name = 'Financiador'
+        verbose_name_plural = 'Financiadores'
 
 class AreasTecnologicas(models.Model):
-    id_area_tecnologica = models.IntegerField(primary_key=True)
-    area_tecnologica = models.CharField(max_length=100)
+    id_area_tecnologica = models.IntegerField(primary_key=True, verbose_name='ID da Área Tecnológica')
+    area_tecnologica = models.CharField(max_length=100, verbose_name='Área Tecnológica')
 
     def __str__(self):
         return self.area_tecnologica
+    
+    class Meta:
+        verbose_name = 'Área Tecnológica'
+        verbose_name_plural = 'Áreas Tecnológicas'
 
 
 class Projetos(models.Model):
-    id_projeto = models.IntegerField(primary_key=True)
-    projeto = models.CharField(max_length=100)
-    id_financiador = models.ForeignKey(Financiador, on_delete=models.CASCADE)
-    id_area_tecnologica = models.ForeignKey(AreasTecnologicas, on_delete=models.CASCADE)
-    coordenador = models.CharField(max_length=100)
-    ativo = models.BooleanField(default=True)
-    inicio_vigencia = models.DateField()
-    fim_vigencia = models.DateField(null=True, blank=True)
-    valor = models.DecimalField(max_digits= 20, decimal_places=2)
-    equipe = models.ManyToManyField(Colaboradores)
+    id_projeto = models.IntegerField(primary_key=True, verbose_name='ID do Projeto')
+    projeto = models.CharField(max_length=100, verbose_name='Projeto')
+    id_financiador = models.ForeignKey(Financiador, on_delete=models.CASCADE, verbose_name='Financiador')
+    id_area_tecnologica = models.ForeignKey(AreasTecnologicas, on_delete=models.CASCADE, verbose_name='Área Tecnológica')
+    coordenador = models.CharField(max_length=100, verbose_name='Coordenador')
+    ativo = models.BooleanField(default=True, verbose_name='Ativo')
+    inicio_vigencia = models.DateField(verbose_name='Início da Vigência')
+    fim_vigencia = models.DateField(null=True, blank=True, verbose_name='Fim da Vigência')
+    valor = models.DecimalField(max_digits= 20, decimal_places=2, verbose_name='Valor')
+    equipe = models.ManyToManyField(Colaboradores, verbose_name='Equipe')
 
     def __str__(self):
         return self.projeto
+    
+    class Meta:
+        verbose_name = 'Projeto'
+        verbose_name_plural = 'Projetos'
 
 
 
